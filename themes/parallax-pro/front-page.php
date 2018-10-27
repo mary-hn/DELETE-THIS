a9ed1d6edd7107d2faabbb0938ac292b11b79aea
mary-hn <44522436+mary-hn@users.noreply.github.com>
2018-10-27 06:54:46 -0400

    Change front page to static page

--- themes/parallax-pro/front-page.php
+++ themes/parallax-pro/front-page.php
@@ -6,13 +6,65 @@
  * @package Parallax
  * @subpackage Customizations
  */
-add_action( 'genesis_meta', 'parallax_home_genesis_meta' );
+/*add_action( 'genesis_meta', 'parallax_home_genesis_meta' );*/
 /**
  * Add widget support for homepage. If no widgets active, display the default loop.
  *
  */
+//* Force full width content layout
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+remove_action( 'genesis_entry_content', 'genesis_do_post_content_nav' );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
 
-function parallax_home_genesis_meta() {
+global $post;
+$post_slug=$post->post_name;
+
+// * Add the featured image after post title
+add_action( 'genesis_before_entry', 'get_involved_featured_image' );
+function get_involved_featured_image() {
+	echo '<div class="programs-hero-image"><div class="wrap"><div class="hero-content"><div class="donate-desc">';
+	echo the_field("donate_text");
+	/*echo '<h3 class="page-title">';
+	echo the_title() . '</h3></div>'; */
+	/* echo the_field("donate_shortcode");*/
+	echo '</div></div>';
+	if ($video_url = get_field("video_url", false, false)) {
+		echo'<video autoplay="" muted="" loop="" width="100%" height="100%" id="myVideo"> <source src="'.$video_url.'" type="video/mp4"> <img src="/wp-content/uploads/2016/12/haiti-now-home-page.jpg" title="Your browser does not support the<video> tag"></video>';
+		echo '</div></div>';
+	}
+	elseif ( $image = genesis_get_image( 'format=url&size=programs' ) ) {
+		printf( '<img class="firstimg" src="%s" alt="%s" />', $image, the_title_attribute( 'echo=0' ) );
+		echo '</div>';
+	} else {
+		echo '<img class="secondimg" src="' . get_bloginfo( 'stylesheet_directory' )
+		. '/images/empty-image.png" /></div>';
+	}
+}
+
+/*add_action( 'genesis_loop', 'get_involved_loop' );
+function get_involved_loop() {
+	global $post;
+	$post_slug=$post->post_name;
+	?>
+
+	<!-- Program Post Images -->
+	<section class="program-posts">
+		<div class="entry">
+			<?php
+			get_template_part( 'template-parts/page/content', 'pages' );
+			?>
+		</div>
+	</section>
+	<?php
+}*/
+
+remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
+remove_action( 'genesis_footer', 'genesis_do_footer' );
+genesis();
+
+
+/*function parallax_home_genesis_meta() {
 
 	if ( is_active_sidebar( 'home-section-1' ) || is_active_sidebar( 'home-section-2' ) || is_active_sidebar( 'home-section-3' ) || is_active_sidebar( 'home-section-4' ) || is_active_sidebar( 'home-section-5' ) || is_active_sidebar( 'home-section-6' ) || is_active_sidebar( 'home-section-7' ) || is_active_sidebar( 'home-section-8' ) || is_active_sidebar( 'home-section-9' ) || is_active_sidebar( 'home-section-10' ) ) {
 
@@ -61,7 +113,12 @@ function parallax_home_genesis_meta() {
 function parallax_homepage_widgets() {
 
 	genesis_widget_area( 'home-section-1', array(
-		'before' => '<div class="home-odd home-section-1 widget-area"><div class="wrap">',
+		'before' => '<div class="home-odd home-section-1 widget-area"><div class="wrap"><video autoplay muted loop  width="100%" height="100%" id="myVideo">
+  <source src="/wp-content/uploads/2018/10/HAITI-NOW-WEBSITE-LOOP_color-draft.webm" type="video/webm">
+  <source src="/wp-content/uploads/2018/10/HAITI-NOW-WEBSITE-LOOP_color_draft.mp4" type="video/mp4">
+  <!--Your browser does not support the video tag. -->
+  <img src="/wp-content/uploads/2016/12/haiti-now-home-page.jpg" title="Your browser does not support the <video> tag">
+</video>',
 		'after'  => '</div></div>',
 		) );
 
@@ -106,4 +163,4 @@ function parallax_homepage_widgets() {
 	) );
 }
 
-genesis();
+genesis();*/
